2cd0fedd8d7d07ab3f8e425f6fc1948349402d23
Hafiyyan Abdul Aziz <hafiyyan.123@gmail.com>
2020-10-14 18:39:54 +0700

    berhasil membuat widget search di main page

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -3,93 +3,148 @@ part of 'pages.dart';
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF14172B),
-      /* appBar: AppBar(
-        title: Text('Cekos'),
-        backgroundColor: Colors.transparent,
-      ), */
-      body: ListView(
-        children: [
-          MainPageSlider(),
-          Container(
-            margin: EdgeInsets.only(
-              left: 25,
-              top: 30,
-              bottom: 10,
-              right: 22,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xFF14172B),
+        /* appBar: AppBar(
+          title: Text('Cekos'),
+          backgroundColor: Colors.transparent,
+        ), */
+        body: Stack(
+          children: [
+            ListView(
               children: [
-                Text(
-                  'Rekomendasi',
-                  style: TextStyle(
-                    fontFamily: 'Rubik',
-                    fontSize: 20,
-                    color: const Color(0xffffffff),
-                    fontWeight: FontWeight.w500,
+                MainPageSlider(),
+                Container(
+                  margin: EdgeInsets.only(
+                    left: 25,
+                    top: 30,
+                    bottom: 10,
+                    right: 22,
                   ),
-                  textAlign: TextAlign.left,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Rekomendasi',
+                        style: TextStyle(
+                          fontFamily: 'Rubik',
+                          fontSize: 20,
+                          color: const Color(0xffffffff),
+                          fontWeight: FontWeight.w500,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          print('kokokoko');
+                        },
+                        child: Text(
+                          'Lihat Semua',
+                          style: TextStyle(
+                            fontFamily: 'Rubik',
+                            fontSize: 15,
+                            color: const Color(0xffffffff),
+                            fontWeight: FontWeight.w500,
+                            decoration: TextDecoration.underline,
+                          ),
+                          textAlign: TextAlign.right,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                GestureDetector(
-                  onTap: () {
-                    print('kokokoko');
-                  },
+                //MainPageRekomendasi(),
+                //MainPageRekomendasi(),
+
+                Container(
+                  margin: EdgeInsets.only(
+                    left: 25,
+                    top: 30,
+                    bottom: 10,
+                  ),
                   child: Text(
-                    'Lihat Semua',
+                    'Promo',
                     style: TextStyle(
                       fontFamily: 'Rubik',
-                      fontSize: 15,
+                      fontSize: 20,
                       color: const Color(0xffffffff),
                       fontWeight: FontWeight.w500,
-                      decoration: TextDecoration.underline,
                     ),
-                    textAlign: TextAlign.right,
+                    textAlign: TextAlign.left,
                   ),
                 ),
+                Container(
+                  margin: EdgeInsets.only(
+                    left: 25,
+                    top: 30,
+                    bottom: 10,
+                  ),
+                  child: Text(
+                    'Perluas Wawasan',
+                    style: TextStyle(
+                      fontFamily: 'Rubik',
+                      fontSize: 20,
+                      color: const Color(0xffffffff),
+                      fontWeight: FontWeight.w500,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                MainPagePerluasWawasan(),
               ],
             ),
-          ),
-          //MainPageRekomendasi(),
-          MainPageRekomendasi(),
-
-          Container(
-            margin: EdgeInsets.only(
-              left: 25,
-              top: 30,
-              bottom: 10,
-            ),
-            child: Text(
-              'Promo',
-              style: TextStyle(
-                fontFamily: 'Rubik',
-                fontSize: 20,
-                color: const Color(0xffffffff),
-                fontWeight: FontWeight.w500,
+            GestureDetector(
+              onTap: () {
+                print('object');
+              },
+              child: Container(
+                margin: EdgeInsets.only(
+                  top: 5,
+                  left: 20,
+                  right: 20,
+                ),
+                height: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: const Color(0xff23243b),
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color(0x80000000),
+                      offset: Offset(4, 4),
+                      blurRadius: 8,
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: Container(
+                    margin: EdgeInsets.only(
+                      left: 20,
+                      right: 10,
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.search,
+                          color: Color(0xFF50E3C2),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Cari Kampus atau Nama Kost',
+                          style: TextStyle(
+                            color: Color(0xFF50E3C2),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
-              textAlign: TextAlign.left,
             ),
-          ),
-          Container(
-            margin: EdgeInsets.only(
-              left: 25,
-              top: 30,
-              bottom: 10,
-            ),
-            child: Text(
-              'Perluas Wawasan',
-              style: TextStyle(
-                fontFamily: 'Rubik',
-                fontSize: 20,
-                color: const Color(0xffffffff),
-                fontWeight: FontWeight.w500,
-              ),
-              textAlign: TextAlign.left,
-            ),
-          ),
-          MainPagePerluasWawasan(),
-        ],
+          ],
+        ),
       ),
     );
 
