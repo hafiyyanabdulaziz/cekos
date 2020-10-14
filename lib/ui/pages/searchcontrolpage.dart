1184315373a606d819c63ecfa3cde8169fd22177
Hafiyyan Abdul Aziz <hafiyyan.123@gmail.com>
2020-10-14 22:25:34 +0700

    design search control appbar selesai

--- lib/ui/pages/searchcontrolpage.dart
+++ lib/ui/pages/searchcontrolpage.dart
@@ -8,53 +8,83 @@ class SearchControlPage extends StatefulWidget {
 class _SearchControlPageState extends State<SearchControlPage> {
   Icon cusIcon = Icon(Icons.search);
   Widget cusSearchBar = Text('Halo');
+  TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: cusSearchBar,
-          actions: [
-            IconButton(
-              icon: cusIcon,
-              onPressed: () {
-                setState(() {
-                  if (this.cusIcon.icon == Icons.search) {
-                    this.cusIcon = Icon(Icons.cancel);
-                    this.cusSearchBar = TextField(
-                      cursorColor: Colors.white,
-                      textInputAction: TextInputAction.go,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Hafo",
-                      ),
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                      ),
-                    );
-                  } else {
-                    this.cusIcon = Icon(Icons.search);
-                    this.cusSearchBar = Text('Halo');
-                  }
-                });
-              },
+    return Scaffold(
+      backgroundColor: Color(0xFF14172B),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF23243B),
+        //centerTitle: true,
+        title: TextField(
+          autofocus: true,
+          onChanged: (value) {
+            setState(() {});
+          },
+          cursorColor: Colors.white,
+          controller: controller,
+          //textInputAction: TextInputAction.go,
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            /* UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+              ),
+            ), */
+            /* suffixIcon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ), */
+            hintText: "Masukkan Nama Kampus atau Kost",
+            hintStyle: TextStyle(
+              color: Color(0xFF50E3C2),
+              fontSize: 13,
             ),
-          ],
+          ),
+          style: TextStyle(
+            color: Colors.white,
+            //height: 1,
+
+            //fontSize: 16.0,
+          ),
         ),
-        body: Center(
-          child: MyWidget(),
+        actions: [
+          IconButton(
+            icon: cusIcon,
+            onPressed: () {
+              /* setState(() {
+                if (this.cusIcon.icon == Icons.search) {
+                  this.cusIcon = Icon(Icons.cancel);
+                  this.cusSearchBar = TextField(
+                    cursorColor: Colors.white,
+                    textInputAction: TextInputAction.go,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Hafo",
+                    ),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),
+                  );
+                } else {
+                  this.cusIcon = Icon(Icons.search);
+                  this.cusSearchBar = Text('Halo');
+                }
+              }); */
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          controller.text,
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
       ),
     );
-  }
-}
-
-class MyWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text('Hello, World!', style: Theme.of(context).textTheme.headline4);
   }
 }
