857eee16da0f349e197f61ec125e11340147aa24
Hafiyyan Abdul Aziz <hafiyyan.123@gmail.com>
2021-01-30 16:40:14 +0700

    perbaikan profile page

--- lib/ui/pages/profilepage.dart
+++ lib/ui/pages/profilepage.dart
@@ -20,10 +20,10 @@ class _ProfilePageState extends State<ProfilePage> {
   //   return sharedPreferences.getString('user_uid');
   // }
 
-  // Future<String> getEmail() async {
-  //   SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-  //   return sharedPreferences.getString('user_email');
-  // }
+  Future<String> getEmail() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    return sharedPreferences.getString('user_email');
+  }
 
   Future<String> getPhoto() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
@@ -37,10 +37,10 @@ class _ProfilePageState extends State<ProfilePage> {
       name = value;
       setState(() {});
     });
-    // getEmail().then((value) {
-    //   email = value;
-    //   setState(() {});
-    // });
+    getEmail().then((value) {
+      email = value;
+      setState(() {});
+    });
     getPhoto().then((value) {
       photo = value;
       setState(() {});
@@ -57,286 +57,188 @@ class _ProfilePageState extends State<ProfilePage> {
       backgroundColor: Color(0xFF14172B),
       appBar: AppBar(
         backgroundColor: Color(0xFF23243B),
-        title: Text(name),
+        title: Text('Profile'),
         centerTitle: true,
-        leading: Container(
-          margin: EdgeInsets.all(10),
-          child: CircleAvatar(
-            //radius: 30,
-            backgroundColor: Color(0xFF23243B),
-            backgroundImage: NetworkImage(photo),
-          ),
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.exit_to_app,
-              color: Colors.red,
-            ),
-            onPressed: () async {
-              CekosGoogleSignIn.signOutGoogle();
-              SharedPreferences sharedPreferences =
-                  await SharedPreferences.getInstance();
-
-              sharedPreferences.clear();
-
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(
-                  builder: (context) {
-                    return SignInPage();
-                  },
-                ),
-              );
-            },
-          )
-        ],
       ),
       body: ListView(
         children: [
-          // Container(
-          //   margin: EdgeInsets.only(
-          //     left: 15,
-          //     top: 15,
-          //     bottom: 15,
-          //     right: 15,
-          //   ),
-          //   child: Row(
-          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //     children: [
-          //       Text(
-          //         'Likes',
-          //         style: TextStyle(
-          //           fontFamily: 'Rubik',
-          //           fontSize: 20,
-          //           color: const Color(0xffffffff),
-          //           fontWeight: FontWeight.w500,
-          //         ),
-          //         textAlign: TextAlign.left,
-          //       ),
-          //       TouchableOpacity(
-          //         child: GestureDetector(
-          //           onTap: () {
-          //             Navigator.of(context).pushReplacement(
-          //               MaterialPageRoute(
-          //                 builder: (context) {
-          //                   return ButtonNavigationBarPage(
-          //                     selectedIndex: 1,
-          //                   );
-          //                 },
-          //               ),
-          //             );
-          //           },
-          //           child: Text(
-          //             'Lihat Semua',
-          //             style: TextStyle(
-          //               fontFamily: 'Rubik',
-          //               fontSize: 15,
-          //               color: const Color(0xffffffff),
-          //               fontWeight: FontWeight.w500,
-          //               decoration: TextDecoration.underline,
-          //             ),
-          //             textAlign: TextAlign.right,
-          //           ),
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
-          // MainPageRekomendasi(),
-          // Divider(
-          //   color: Color(0xFF50E3C2),
-          // ),
-          // Container(
-          //   margin: EdgeInsets.only(
-          //     left: 15,
-          //     top: 15,
-          //     bottom: 15,
-          //     right: 15,
-          //   ),
-          //   child: Row(
-          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //     children: [
-          //       Text(
-          //         'Survey',
-          //         style: TextStyle(
-          //           fontFamily: 'Rubik',
-          //           fontSize: 20,
-          //           color: const Color(0xffffffff),
-          //           fontWeight: FontWeight.w500,
-          //         ),
-          //         textAlign: TextAlign.left,
-          //       ),
-          //       TouchableOpacity(
-          //         child: GestureDetector(
-          //           onTap: () {
-          //             Navigator.of(context).pushReplacement(
-          //               MaterialPageRoute(
-          //                 builder: (context) {
-          //                   return ButtonNavigationBarPage(
-          //                     selectedIndex: 1,
-          //                   );
-          //                 },
-          //               ),
-          //             );
-          //           },
-          //           child: Text(
-          //             'Lihat Semua',
-          //             style: TextStyle(
-          //               fontFamily: 'Rubik',
-          //               fontSize: 15,
-          //               color: const Color(0xffffffff),
-          //               fontWeight: FontWeight.w500,
-          //               decoration: TextDecoration.underline,
-          //             ),
-          //             textAlign: TextAlign.right,
-          //           ),
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
-          // // MainPageRekomendasi(),
-          // Divider(
-          //   color: Color(0xFF50E3C2),
-          // ),
-          // Container(
-          //   margin: EdgeInsets.only(
-          //     left: 15,
-          //     top: 15,
-          //     bottom: 15,
-          //     right: 15,
-          //   ),
-          //   child: Row(
-          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //     children: [
-          //       Text(
-          //         'Booking',
-          //         style: TextStyle(
-          //           fontFamily: 'Rubik',
-          //           fontSize: 20,
-          //           color: const Color(0xffffffff),
-          //           fontWeight: FontWeight.w500,
-          //         ),
-          //         textAlign: TextAlign.left,
-          //       ),
-          //       TouchableOpacity(
-          //         child: GestureDetector(
-          //           onTap: () {
-          //             Navigator.of(context).pushReplacement(
-          //               MaterialPageRoute(
-          //                 builder: (context) {
-          //                   return ButtonNavigationBarPage(
-          //                     selectedIndex: 1,
-          //                   );
-          //                 },
-          //               ),
-          //             );
-          //           },
-          //           child: Text(
-          //             'Lihat Semua',
-          //             style: TextStyle(
-          //               fontFamily: 'Rubik',
-          //               fontSize: 15,
-          //               color: const Color(0xffffffff),
-          //               fontWeight: FontWeight.w500,
-          //               decoration: TextDecoration.underline,
-          //             ),
-          //             textAlign: TextAlign.right,
-          //           ),
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
-          // // MainPageRekomendasi(),
-          // Divider(
-          //   color: Color(0xFF50E3C2),
-          // ),
-          ListView(
-            shrinkWrap: true,
-            physics: ClampingScrollPhysics(),
-            children: ListTile.divideTiles(
-                context: context,
-                color: Color(0xFF50E3C2),
-                tiles: [
-                  ListTile(
-                    title: Text(
-                      'Kebijakan Privasi',
+          Container(
+            height: 10,
+          ),
+          //isi profile
+          Container(
+            //height: 200,
+            color: Color(0xff23243b),
+            child: Center(
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 20,
+                      bottom: 10,
+                    ),
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
+                      image: DecorationImage(
+                        image: NetworkImage(photo),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      name,
                       style: TextStyle(
                         fontFamily: 'Rubik',
                         fontSize: 20,
-                        color: const Color(0xffffffff),
-                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
                       ),
                     ),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return WebviewDetail(
-                                link:
-                                    'https://cekos.kitahukomputer.com/webview/kebijakanprivasi.html',
-                                textAppBar: 'Kebijakan Privasi');
-                          },
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 5,
+                      bottom: 20,
+                    ),
+                    child: Text(
+                      email,
+                      style: TextStyle(
+                        fontFamily: 'Rubik',
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            color: Color(0xff23243b),
+            child: ListView(
+              shrinkWrap: true,
+              physics: ClampingScrollPhysics(),
+              children: ListTile.divideTiles(
+                  context: context,
+                  color: Color(0xFF50E3C2),
+                  tiles: [
+                    ListTile(
+                      title: Text(
+                        'Kebijakan Privasi',
+                        style: TextStyle(
+                          fontFamily: 'Rubik',
+                          fontSize: 20,
+                          color: const Color(0xffffffff),
+                          fontWeight: FontWeight.w500,
                         ),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    title: Text(
-                      'Tentang Cekos',
-                      style: TextStyle(
-                        fontFamily: 'Rubik',
-                        fontSize: 20,
-                        color: const Color(0xffffffff),
-                        fontWeight: FontWeight.w500,
                       ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return WebviewDetail(
+                                  link:
+                                      'https://cekos.kitahukomputer.com/webview/kebijakanprivasi.html',
+                                  textAppBar: 'Kebijakan Privasi');
+                            },
+                          ),
+                        );
+                      },
                     ),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return WebviewDetail(
-                                link:
-                                    'https://cekos.kitahukomputer.com/webview/tentangcekos.html',
-                                textAppBar: 'Tentang Cekos');
-                          },
+                    ListTile(
+                      title: Text(
+                        'Tentang Cekos',
+                        style: TextStyle(
+                          fontFamily: 'Rubik',
+                          fontSize: 20,
+                          color: const Color(0xffffffff),
+                          fontWeight: FontWeight.w500,
                         ),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    title: Text(
-                      'Hubungi Developer',
-                      style: TextStyle(
-                        fontFamily: 'Rubik',
-                        fontSize: 20,
-                        color: const Color(0xffffffff),
-                        fontWeight: FontWeight.w500,
                       ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return WebviewDetail(
+                                  link:
+                                      'https://cekos.kitahukomputer.com/webview/tentangcekos.html',
+                                  textAppBar: 'Tentang Cekos');
+                            },
+                          ),
+                        );
+                      },
                     ),
-                    onTap: () async {
-                      await launch('https://wa.me/6281283793435');
-                    },
-                  ),
-                  ListTile(
-                    title: Text(
-                      'Bantu Kembangkan',
-                      style: TextStyle(
-                        fontFamily: 'Rubik',
-                        fontSize: 20,
-                        color: const Color(0xffffffff),
-                        fontWeight: FontWeight.w500,
+                    ListTile(
+                      title: Text(
+                        'Hubungi Developer',
+                        style: TextStyle(
+                          fontFamily: 'Rubik',
+                          fontSize: 20,
+                          color: const Color(0xffffffff),
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
+                      onTap: () async {
+                        await launch('https://wa.me/6281283793435');
+                      },
                     ),
-                    onTap: () async {
-                      await launch(
-                          'https://github.com/hafiyyanabdulaziz?tab=repositories&q=cekos');
+                    ListTile(
+                      title: Text(
+                        'Bantu Kembangkan',
+                        style: TextStyle(
+                          fontFamily: 'Rubik',
+                          fontSize: 20,
+                          color: const Color(0xffffffff),
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      onTap: () async {
+                        await launch(
+                            'https://github.com/hafiyyanabdulaziz?tab=repositories&q=cekos');
+                      },
+                    ),
+                  ]).toList(),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              left: 20,
+              right: 20,
+              top: 10,
+            ),
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5)),
+              color: Colors.red,
+              onPressed: () async {
+                CekosGoogleSignIn.signOutGoogle();
+                SharedPreferences sharedPreferences =
+                    await SharedPreferences.getInstance();
+
+                sharedPreferences.clear();
+
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return SignInPage();
                     },
                   ),
-                ]).toList(),
+                );
+              },
+              child: Text(
+                'Logout',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
           ),
           Container(
             margin: EdgeInsets.all(10),
